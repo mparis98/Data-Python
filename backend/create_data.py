4a742e39aa2be9253314e3ea546e4923b87473af
Matthieu <parismatthieu@yahoo.fr>
2020-09-08 11:39:49 +0200

    Get datas json

--- backend/create_data.py
+++ backend/create_data.py
@@ -1,13 +1,38 @@
-import requests
+import requests, json, urllib
 
+url = "https://opendata.datainfogreffe.fr/api/records/1.0/search/?dataset=societes-immatriculees-2020&q=&rows=2&sort=date_immatriculation&facet=siren&facet=forme_juridique&facet=ville&facet=region&facet=date_immatriculation&facet=statut"
 
-datas = [
-    {'name': 'Blackberry'},
-    {'name': 'Bip'},
-    {'name': 'Walkman'},
-    {'name': 'Discman'},
-]
-
+r = requests.get(url)
+datas = r.json()
+mylist = []
+for data in datas['records']:
+    if 'region' in data['fields'].keys():
+        region = data['fields']['region']
+    else:
+        region = None
+    if 'ville' in data['fields'].keys():
+        ville = data['fields']['ville']
+    else:
+        ville = None
+    if 'code_postal' in data['fields'].keys():
+        code_postal = data['fields']['code_postal']
+    else:
+        code_postal = None
+    if 'num_dept' in data['fields'].keys():
+        num_dept = data['fields']['num_dept']
+    else:
+        num_dept = None
+        
+    mylist.append( {
+        'siren':data['fields']['siren'],
+        'denomination':data['fields']['denomination'],
+        'region':region,
+        'ville':ville,
+        'code_postal':code_postal,
+        'num_dept':num_dept,
+        'date_immatriculation':data['fields']['date_immatriculation']
+    })
+print(mylist)
 
 for data in datas:
     requests.post('http://localhost:5000/company/add', data=data)

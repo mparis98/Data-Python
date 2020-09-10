c9deaee8db96ba69e93a49fd3efeb5c645e468b6
Matthieu <parismatthieu@yahoo.fr>
2020-09-10 10:36:16 +0200

    Update company

--- backend/create_data.py
+++ backend/create_data.py
@@ -1,15 +1,11 @@
 import requests, json, urllib
 
-url = "https://opendata.datainfogreffe.fr/api/records/1.0/search/?dataset=societes-immatriculees-2020&q=&rows=2500&sort=date_immatriculation&facet=siren&facet=forme_juridique&facet=code_ape&facet=ville&facet=region&facet=greffe&facet=date_immatriculation&facet=statut"
+url = "https://opendata.datainfogreffe.fr/api/records/1.0/search/?dataset=societes-immatriculees-2020&q=&rows=10000&sort=date_immatriculation&facet=siren&facet=forme_juridique&facet=code_ape&facet=ville&facet=region&facet=greffe&facet=date_immatriculation&facet=statut&refine.date_immatriculation=2020-06"
 
 r = requests.get(url)
 datas = r.json()
 mylist = []
 for data in datas['records']:
-    if 'region' in data['fields'].keys():
-        region = data['fields']['region']
-    else:
-        region = None
     if 'ville' in data['fields'].keys():
         ville = data['fields']['ville']
     else:
@@ -22,12 +18,12 @@ for data in datas['records']:
         num_dept = data['fields']['num_dept']
     else:
         num_dept = None
-    if 'code_ape' in data['fields'].keys() and 'siren' in data['fields'].keys():
+    if 'code_ape' in data['fields'].keys() and 'siren' in data['fields'].keys() and 'region' in data['fields'].keys():
         code_ape = data['fields']['code_ape']
         mylist.append({
             'siren':data['fields']['siren'],
             'denomination':data['fields']['denomination'],
-            'region':region,
+            'region':data['fields']['region'],
             'ville':ville,
             'code_postal':code_postal,
             'num_dept':num_dept,

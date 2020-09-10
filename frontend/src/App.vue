4292bc967970f37a0ceba9caa72d76e8d045513e
scoorp29 <scorp31.scorp31@gmail.com>
2020-09-10 13:37:45 +0200

    Add navigation, update company table

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <img height="100px" src="./assets/flask-logo.png">
-    <img height="100px" src="./assets/logo.png">
-    <!-- <HelloWorld/> -->
-    <Company/>
-  </div>
+<div id="app">
+    <img id="logo" src="./assets/logo.png">
+    <Company />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 import Company from './components/Company'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Company
-  }
+    name: 'App',
+    components: {
+        Company
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+}
+
+#logo {
+    position: fixed;
+    top: 20px;
+    left: 20px;
+    height: 60px;
+    z-index: 99;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
 }
 </style>
--- frontend/src/components/Company.vue
+++ frontend/src/components/Company.vue
@@ -1,121 +1,163 @@
 <template>
-  <div class="product">
-    <h1>Entreprises</h1>
-    <table id="example" class="display" style="width:100%">
-      <thead>
-            <tr>
-                <th>Siren</th>
-                <th>Denomination</th>
-                <th>Region</th>
-                <th>Ville</th>
-                <th>Code Postal</th>
-                <th>Numéro de département</th>
-                <th>Date d'immatriculation</th>
-                <th>Code APE </th>
-                <th>Fiche identité </th> 
-            </tr>
-        </thead>
-    </table>
-  </div>
+<div id="app">
+    <ul id="menu">
+        <li data-menuanchor="page1" class="active"><a href="#page1">Section 1</a></li>
+        <li data-menuanchor="page2"><a href="#page2">Section 2</a></li>
+        <li data-menuanchor="page3"><a href="#page3">Entreprise 2020</a></li>
+        <li>
+            <a href="https://github.com/mparis98/Data-Python" target="_blank" rel="noopener" class="twitter-share">
+                <img height="25px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png">
+            </a>
+        </li>
+    </ul>
+
+    <full-page :options="options" id="fullpage">
+        <div class="section">
+            <h3>Section 1</h3>
+        </div>
+        <div class="section">
+            <div class="slide">
+                <h3>Slide 2.1</h3>
+            </div>
+            <div class="slide">
+                <h3>Slide 2.2</h3>
+            </div>
+            <div class="slide">
+                <h3>Slide 2.3</h3>
+            </div>
+        </div>
+        <div class="section">
+            <h1>Entreprises Juin 2020</h1>
+            <grid :auto-width="autoWidth" :cols="cols" :pagination="pagination" :rows="companys" :search="search" :sort="sort"></grid>
+        </div>
+    </full-page>
+</div>
 </template>
 
 <script>
-
 import axios from 'axios';
+import Grid from '../../node_modules/gridjs-vue/src/gridjs-vue';
 
 export default {
-  data () {
-    return {
-      name: '',
-      editCompany: {
-        name: ''
-      },
-      companys: [],
-      url: 'http://localhost:5000/company/'
-    }
-  },
-  methods: {
-    getCompanys(){
-      axios.get(this.url)
-      .then((result) => {
-        this.companys = result.data.data
-      })
+    components: {
+        Grid
     },
-    addCompany(){
-      let data = {'name': this.name}
-      axios.post(this.url, data)
-      .then((result) => {
-        this.companys.push(
-          {
-            id: result.data.data.id,
-            name: result.data.data.name
-          }
-        )
-        this.name = ''
-      })
-    },
-    remove(company){
-      axios.delete(this.url + company.id)
-      .then((response) => {
-        var idx = this.companys.indexOf(company)
-        this.companys.splice(idx, 1)
-      })
-    }
-  },
-  mounted(){
-    this.getCompanys()
-  }
-}
-</script>
-
-<script>
-$(document).ready(function() {
-      var t = $('#example').DataTable();
-      var cpt = 0;
-  $.ajax({
-            method: 'GET',
-            type: 'GET',
+    data() {
+        return {
+            companys: [],
+            autoWidth: true,
+            pagination: {
+                limit: 8
+            },
+            search: true,
+            sort: true,
             url: 'http://localhost:5000/company/',
-            success: function (datas) {
-              datas.data.forEach(element => {
-                if (cpt < 1000){
-                  t.row.add([
-                    element.siren,
-                    element.denomination,
-                    element.region,
-                    element.ville,
-                    element.code_postal,
-                    element.num_dept,
-                    element.date_immatriculation,
-                    element.code_ape,
-                    `<a target="_blank" href="${element.fiche_identite}">${element.fiche_identite}</a>`
-                ]).draw( false );
-                cpt++;
-                }
-              })
+            cols: ['Siren', 'Denomination', 'Region', 'Ville', 'Code_Postal', 'Date_Immatriculation', 'Code_Ape'],
+            options: {
+                licenseKey: 'YOUR_KEY_HERE',
+                afterLoad: this.afterLoad,
+                scrollOverflow: true,
+                scrollBar: false,
+                menu: '#menu',
+                navigation: true,
+                anchors: ['page1', 'page2', 'page3'],
+                sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#ee1a59', '#2c3e4f', '#ba5be9', '#b4b8ab']
             }
-  });
-} );
+        }
+    },
+    methods: {
+        afterLoad() {
+            console.log('After load')
+        },
+        addSection(e) {
+            e.preventDefault()
+            var newSectionNumber = document.querySelectorAll('.fp-section').length + 1
+            // creating the section div
+            var section = document.createElement('div')
+            section.className = 'section'
+            section.innerHTML = `<h3>Section ${newSectionNumber}</h3>`
+            // adding section
+            document.querySelector('#fullpage').appendChild(section)
+            // creating the section menu element
+            var sectionMenuItem = document.createElement('li')
+            sectionMenuItem.setAttribute('data-menuanchor', 'page' + newSectionNumber)
+            sectionMenuItem.innerHTML = `<a href="#page${newSectionNumber}">Section${newSectionNumber}</a>`
+            // adding it to the sections menu
+            var sectionsMenuItems = document.querySelector('#menu')
+            sectionsMenuItems.appendChild(sectionMenuItem)
+            // adding anchor for the section
+            this.options.anchors.push(`page${newSectionNumber}`)
+            // we have to call `update` manually as DOM changes won't fire updates
+            // requires the use of the attribute ref="fullpage" on the
+            // component element, in this case, <full-page>
+            // ideally, use an ID element for that element too
+            this.$refs.fullpage.build()
+        },
+        removeSection() {
+            var sections = document.querySelector('#fullpage').querySelectorAll('.fp-section')
+            var lastSection = sections[sections.length - 1]
+            // removing the last section
+            lastSection.parentNode.removeChild(lastSection)
+            // removing the last anchor
+            this.options.anchors.pop()
+            // removing the last item on the sections menu
+            var sectionsMenuItems = document.querySelectorAll('#menu li')
+            var lastItem = sectionsMenuItems[sectionsMenuItems.length - 1]
+            lastItem.parentNode.removeChild(lastItem)
+        },
+        toggleNavigation() {
+            this.options.navigation = !this.options.navigation
+        },
+        toggleScrollbar() {
+            console.log('Changing scrollbar...')
+            this.options.scrollBar = !this.options.scrollBar
+        },
+        getCompanys() {
+            axios.get(this.url)
+                .then((result) => {
+                    this.companys = result.data.data
+                })
+        },
+    },
+    mounted() {
+        this.getCompanys()
+    }
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style>
+#menu {
+    position: fixed;
+    width: 100%;
+    top: 20px;
+    right: 20px;
+    z-index: 70;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
+    letter-spacing: 1px;
+    font-size: 1.1em;
 }
+
+h1 {
+    color: #fff;
+}
+
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+    color: #fff;
+    text-decoration: none;
 }
-table {
-  margin-left: auto;
-  margin-right: auto;
+
+ul .active {
+    text-decoration: underline;
 }
 </style>
